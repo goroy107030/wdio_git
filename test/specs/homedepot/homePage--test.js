dfc993a291e4ddcbe0ea1d720f6f9c4b7b120a3e
Gouranga <roygouranga30@gmail.com>
2022-07-20 10:05:37 +0530

    Adding node modules

---
+++ test/specs/homedepot/homePage--test.js
@@ -0,0 +1,87 @@
+describe('Features: Validating homedepot site', () => {
+
+    before(() => {
+        console.log('Starting execution');
+    });
+
+
+    after(() => {
+        console.log('Windows are closing');
+        browser.closeWindow();
+    });
+
+
+    describe.only('Validating the search box', () => {
+        it('Given: User Navigate to Homedepot site', () => {
+            browser.deleteAllCookies();
+            browser.url('https://www.homedepot.com');
+            browser.maximizeWindow();
+            browser.pause('2000');
+        });
+
+        it('When: User search for Hammer', () => {
+            const searchBox = $('#headerSearch');
+            const searchButton = $('#headerSearchButton');
+            searchBox.setValue('Hammer');
+            searchButton.click();
+            browser.pause('2000');
+        });
+        it('Then: User get the result', () => {
+            const result= $('.results-wrapped');
+            expect(result).toBeExisting();
+            //expect(browser).toHaveUrlContaining('Hammer')
+            browser.execute('window.scrollTo(0,(document.body.scrollHeight)/3)');
+            browser.pause('2000');           
+            browser.execute('window.scrollTo(0,document.body.scrollHeight)');
+            browser.pause('2000');
+
+        });
+        it('And: User validate the result count is mathcing',()=>{
+            const resultsArray = $$(".results-wrapped [id*=browse-search-pods-] [data-automation-id='podnode']");
+            console.log('The Result Length is ',resultsArray.length);
+        })
+
+    });
+    describe('Validating the search box2', () => {
+        it('Given: User Navigate to Homedepot site again', () => {
+            browser.deleteAllCookies();
+            browser.url('https://www.homedepot.com');
+            browser.maximizeWindow();
+            browser.pause('2000');
+        });
+
+        it('When: User search for Pliers', () => {
+            const searchBox = $('#headerSearch');
+            const searchButton = $('#headerSearchButton');
+            searchBox.setValue('Pliers');
+            searchButton.click();
+            browser.pause('2000');
+        });
+        it('Then: User get the result', () => {
+
+
+        });
+
+    });
+
+
+    /*
+        describe('Validating the header', () => {
+            
+        });
+    
+    
+        describe('Validating the footer', () => {
+            
+        });
+    
+    
+        describe('Validating the page title', () => {
+            
+        });
+        */
+
+
+
+
+})
---
+++ test/specs/homedepot/homepage-pip--test.js
@@ -0,0 +1,12 @@
+describe("Features: Validation the PIP Page",()=>{
+
+    describe('Scenario 1', () => {
+        it('Validate Product Sku', () => {
+            
+        });
+        
+    });
+
+
+
+})
